5d0f4aa4f71cad9ca72dbada31bcdfe2df8a2ed6
Seb35 <seb35@seb35.fr>
2016-06-17 16:43:03 +0200

    Création de l’utilisateur s’il n’existe pas
    Et ajout d’une entrée dans le journal des nouveaux utilisateurs

--- MGWiki.php
+++ MGWiki.php
@@ -27,9 +27,10 @@ class MGWiki {
 
 	/**
 	 * Display a warning if the user account and user page don’t together exist or are missing.
-	 * 
+	 *
 	 * @param Title $targetTitle Page title
 	 * @param string $pre_form_html String displayed just before the form
+	 * @return true
 	 */
 	public static function onsfHTMLBeforeForm( $targetTitle, &$pre_form_html ) {
 
@@ -41,7 +42,7 @@ class MGWiki {
 
 		if( $targetTitle->exists() xor $user ) {
 
-			$pre_form_html = '<div class="infoMessage">';
+			$pre_form_html = '<div class="warningbox">';
 			if( $targetTitle->exists() ) $pre_form_html .= wfMessage( 'userpage-without-useraccount' )->escaped();
 			else $pre_form_html .= wfMessage( 'useraccount-without-userpage' )->escaped();
 			$pre_form_html .= "</div>\n";
@@ -55,30 +56,60 @@ class MGWiki {
 		
 	}
 
+	/**
+	 * When a user page is modified by SemanticMediaWiki, create the corresponding MediaWiki user or update the email
+	 *
+	 * Only the user or 'admins' with the right 'mgwikimanageusers' can report the email address in the user preferences.
+	 * If $wgNewUserLog is true (default), add an entry in the 'newusers' log when a user is created.
+	 * 
+	 * @param SMWSQLStore3 $store SemanticMediaWiki store
+	 * @param SMWSemanticData $semanticData Semantic data
+	 * @param SMW\SQLStore\CompositePropertyTableDiffIterator $compositePropertyTableDiffIterator Differences on property values
+	 * @return true
+	 */
 	public static function onSMW_SQLStore_AfterDataUpdateComplete( SMWSQLStore3 $store, SMWSemanticData $semanticData, SMW\SQLStore\CompositePropertyTableDiffIterator $compositePropertyTableDiffIterator ) {
 
-		global $wgUser;
+		global $wgUser, $wgNewUserLog;
 
 		# Get user
 		$subject = $semanticData->getSubject();
 		if( $subject->getNamespace() != NS_USER ) return;
 		$user = User::newFromName( $subject->getDBkey() );
-		if( !$user->getId() ) return;
 
 		# Check permissions
-		if( $wgUser->getId() != $user->getId() && !$wgUser->isAllowed( 'mgwikimanageusers' ) ) return;
+		if( ($wgUser->getId() && $wgUser->getId() != $user->getId()) && !$wgUser->isAllowed( 'mgwikimanageusers' ) ) return;
 
 		# Get properties after they are saved
 		$properties = $semanticData->getProperties();
 		if( count( $properties ) == 0 ) return;
 
-		# Search if there is an E-mail property, and if so update the email preference
+		# Search if there is an E-mail property
+		$email = '';
 		if( array_key_exists( 'E-mail', $properties ) ) {
 			$emailValues = $semanticData->getPropertyValues( $properties['E-mail'] );
 			if( count( $emailValues ) == 0 || current( $emailValues )->getDIType() != SMWDataItem::TYPE_BLOB ) return;
 			$email = current( $emailValues )->getString();
+		}
+
+		# If the user doesn’t exist, create it
+		if( !$user->getId() ) {
+			$user = User::createNew( $subject->getDBkey(), array( 'email' => $email ) );
+			if( $wgNewUserLog ) {
+				$logEntry = new \ManualLogEntry( 'newusers', 'create2' );
+				$logEntry->setPerformer( $wgUser );
+				$logEntry->setTarget( $user->getUserPage() );
+				$logEntry->setParameters( array( '4::userid' => $user->getId() ) );
+				$logid = $logEntry->insert();
+				$logEntry->publish( $logid );
+			}
+		}
+		# Or just update the email
+		elseif( $email ) {
+			if( $wgUser->getEmail() == $email ) return;
 			$wgUser->setEmail( $email );
 			$wgUser->saveSettings();
 		}
+
+		return true;
 	}	
 }
