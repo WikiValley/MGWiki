79a6387c7fe6c7e49e154ff0d5348c5f6789463b
Seb35 <seb35@seb35.fr>
2019-11-14 21:12:37 +0100

    Ajout de 2 actions API pour ajouter des groupes depuis l’ADEPUL
    1/ Action 'nouveau-groupe-adepul' : ajoute une page de groupe DPC
    ADEPUL, crée l’utilisateur formateur si besoin
    2/ Action 'membre-groupe-adepul' : ajoute un ou plusieurs membres
    ADEPUL dans un groupe précédemment créé, crée les utilisateurs si
    besoin
    
    Les utilisateurs, lorsque inexistants, sont créés selon la procédure
    spécifique à MGWiki (initialisation de la page utilisateur et envoi
    d’un lien unique de connexion par email).
    
    Ces deux actions utilisent deux propriétés Semantic MediaWiki pour
    retrouver un utilisateur à partir de son ID ADEPUL stocké dans une
    propriété sur la page utilisateur, et pour retrouver un groupe ADEPUL
    à partir de son ID stocké dans une propriété sur la page du groupe.
    Ces deux propriétés sont définies dans extension.json.

--- ApiNewADEPULGroup.php
+++ ApiNewADEPULGroup.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2007 Iker Labarga "<Firstname><Lastname>@gmail.com"
+ * Copyright © 2019 Wiki Valley
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,15 +24,11 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\RevisionRecord;
 
 /**
- * A module that allows for editing and creating pages.
- *
- * Currently, this wraps around the EditPage class in an ugly way,
- * EditPage.php should be rewritten to provide a cleaner interface,
- * see T20654 if you're inspired to fix this.
+ * A module that creates a new ADEPUL group.
  *
  * @ingroup API
  */
-class ApiAddGroupADEPUL extends ApiBase {
+class ApiNewADEPULGroup extends ApiBase {
 	public function execute() {
 		global $wgMGWikiSecretKeyADEPUL;
 		$params = $this->extractRequestParams();
@@ -44,35 +40,41 @@ class ApiAddGroupADEPUL extends ApiBase {
 		$formateur = $params['formateur'];
 		$cle = $params['cle'];
 
-		if( $cle !== $wgMGWikiSecretKeyADEPUL ) {
+		if( $wgMGWikiSecretKeyADEPUL === null || !hash_equals( $wgMGWikiSecretKeyADEPUL, $cle ) ) {
 			$this->dieWithError( 'mgwiki-wrong-secret-key' );
 		}
 
 		$annee = intval( preg_replace( '/^[A-Z]([0-9]{2}).*/', '$1', $code_action ) );
 		$annee = $annee ? '20' . $annee : '';
 
-		$formateurUser = $this->getUserByADEPUL( $formateurAdepul );
+		try {
+			$formateurUser = MGWiki::getUserByADEPUL( $formateur );
+			$formateurUsername = $formateurUser->getName();
+		} catch( Exception $e ) {
+			$this->dieWithError( 'mgwiki-wrong-secret-key' );
+		}
 
 		$title = "DPC ADEPUL $annee - $intitule_ac";
 		$wikitext = "{{Groupe
 |Archivé=Non
 |Description=$intitule_long_ac
 |Institution de rattachement=ADEPUL
-|Tuteur ou modérateur=$formateurUser
+|Tuteur ou modérateur=$formateurUsername
 |Membres=
 |Type de groupe=DPC
 |Titre de la formation=$intitule_ac
 |Année=$annee
+|Code action=$code_action
 }}
 {{Groupe2}}";
-		$groupTitle = $userTitle = Title::newFromText( $title, NS_MAIN );
+		$groupTitle = Title::newFromText( $title, NS_MAIN );
 		$groupArticle = WikiPage::factory( $groupTitle );
 		$content = new WikitextContent( $wikitext );
 		$summary = wfMessage( 'mgwiki-create-grouppage' )->inContentLanguage()->text();
 		$flags = EDIT_NEW;
 		$groupArticle->doEditContent( $content, $summary, $flags, false, $formateurUser );
 
-		$apiResult->addValue( null, $this->getModuleName(), $r );
+		$apiResult->addValue( null, "result", "success" );
 	}
 
 	public function mustBePosted() {
@@ -109,54 +111,5 @@ class ApiAddGroupADEPUL extends ApiBase {
 
 	public function needsToken() {
 		return false;
-	}
-
-	public function getUserADEPUL( $code_adepul ) {
-		global $wgMGWikiUserEndpointADEPUL, $wgMGWikiSecretKeyADEPUL;
-		$infoADEPUL = file_get_contents( $wgMGWikiUserEndpointADEPUL . '?t_idf=' . $code_adepul . '&cle=' . $wgMGWikiSecretKeyADEPUL );
-		if( !$infoADEPUL ) {
-			$this->dieWithError( 'mgwiki-wrong-secret-key' );
-		}
-		$infoADEPUL = json_decode( $infoADEPUL );
-		if( $infoADEPUL === null ) {
-			$this->dieWithError( 'mgwiki-wrong-secret-key' );
-		}
-		if( $infoADEPUL->existe === 'NON' ) {
-			return false;
-		}
-		return $infoADEPUL;
-	}
-
-	public function getUserByADEPUL( $code_adepul ) {
-		global $wgUser;
-		global $wgMGWikiUserProperties;
-		list( $query, $params ) = SMWQueryProcessor::getQueryAndParamsFromFunctionParams( [ "[[{$wgMGWikiUserProperties['codeAdepul']}::$code_adepul]]" ], SMW_OUTPUT_RAW, 1000, false );
-		$result = SMWQueryProcessor::getResultFromQuery( $query, $params, SMW_OUTPUT_RAW, null );
-		$matches = null;
-		if( preg_match( '/^\[\[(.*)\]\]$/', $result, $matches ) ) {
-			return $matches[1];
-		} else {
-			return;
-			# Create user
-			$backupWgUser = $wgUser;
-			$wgUser = User::newFromName( 'Alexandre BRULET' );
-			$adhAdepul = self::getUserADEPUL( $code_adepul );
-			$prenom = $adhAdepul->prenom;
-			$nom = $adhAdepul->nom;
-			$mail = $adhAdepul->mail;
-			$profession = $adhAdepul->profession;
-			$specialite = $adhAdepul->specialite;
-			$username = $prenom . ' ' . strtoupper( $nom );
-			$userData = [];
-			$userData[$wgMGWikiUserProperties['firstname']] = $prenom;
-			$userData[$wgMGWikiUserProperties['lastname']] = $nom;
-			$userData[$wgMGWikiUserProperties['institution']] = 'ADEPUL';
-			$userData[$wgMGWikiUserProperties['codeAdepul']] = $code_adepul;
-			$userData[$wgMGWikiUserProperties['email']] = $mail;
-			#$userData[$wgMGWikiUserProperties['']] = ;
-			MGWiki::createUser( $username, $userData );
-			$wgUser = $backupWgUser;
-			return $username;
-		}
 	}
 }
--- MGWiki.php
+++ MGWiki.php
@@ -550,7 +550,7 @@ class MGWiki {
 	 * @param array $groups Groups
 	 * @return bool The user was created
 	 */
-	protected static function createUser( $username, $userData = [], array $groups = [] ) {
+	public static function createUser( $username, $userData = [], array $groups = [] ) {
 
 		global $wgUser, $wgNewUserLog, $wgVersion;
 		global $wgMGWikiUserProperties;
@@ -623,5 +623,184 @@ class MGWiki {
 		$user->sendConfirmationMail( 'created_by_mgwiki' );
 
 		return true;
+	}
+
+	/**
+	 * Get the user from the official ADEPUL database.
+	 *
+	 * @param string $code_adepul ADEPUL id.
+	 * @return object|false|null Object with the various data describing the ADEPUL user, or false if non-existing ADEPUL user, or null if error.
+	 */
+	public static function getUserFromOfficialADEPUL( $code_adepul ) {
+		global $wgMGWikiUserEndpointADEPUL, $wgMGWikiSecretKeyADEPUL;
+		$infoADEPUL = file_get_contents( $wgMGWikiUserEndpointADEPUL . '?t_idf=' . $code_adepul . '&cle=' . $wgMGWikiSecretKeyADEPUL );
+		if( !$infoADEPUL ) {
+			return null;
+		}
+		$infoADEPUL = json_decode( $infoADEPUL );
+		if( $infoADEPUL === null ) {
+			return null;
+		}
+		if( $infoADEPUL->existe === 'NON' ) {
+			return false;
+		}
+		return $infoADEPUL;
+	}
+
+	/**
+	 * Get (or create) the user corresponding to an ADEPUL id.
+	 *
+	 * @param string $code_adepul ADEPUL id.
+	 * @param string|null $creator Username creating a new user, or $wgMGWikiDefaultCreatorNewAccounts if null.
+	 * @return User|null MediaWiki user, possibly just created with the specific MGWiki process.
+	 */
+	public static function getUserByADEPUL( $code_adepul, $creator = null ) {
+		global $wgUser;
+		global $wgMGWikiUserProperties, $wgMGWikiDefaultCreatorNewAccounts;
+
+		$codeAdepulTitle = Title::newFromText( 'Property:' . $wgMGWikiUserProperties['codeAdepul'] );
+		$codeAdepul = $codeAdepulTitle->getDBkey();
+
+		// Create property instance
+		$property = new SMWDIProperty( $codeAdepul );
+		$property->setPropertyTypeId( SMW\DataValues\StringValue::TYPE_ID );
+		$dataItem = new SMWDIBlob( $code_adepul );
+		$dataValue = SMW\DataValueFactory::getInstance()->newDataValueByItem(
+			$dataItem,
+			$property
+		);
+
+		// Create a description that represents the condition
+		$descriptionFactory = new SMW\Query\DescriptionFactory();
+		$namespaceDescription = $descriptionFactory->newNamespaceDescription(
+			NS_USER
+		);
+		$descriptionAdepul = $descriptionFactory->newSomeProperty(
+			$property,
+			$descriptionFactory->newValueDescription( $dataItem )
+		);
+		$description = $descriptionFactory->newConjunction( array(
+			$namespaceDescription,
+			$descriptionAdepul
+		) );
+		$propertyValue = SMW\DataValueFactory::getInstance()->newPropertyValueByLabel(
+			$codeAdepul
+		);
+
+		$description->addPrintRequest(
+			new SMW\Query\PrintRequest( SMW\Query\PrintRequest::PRINT_PROP, null, $propertyValue )
+		);
+
+		// Create query object
+		$query = new SMWQuery(
+			$description
+		);
+
+		$query->querymode = SMWQuery::MODE_INSTANCES;
+
+		// Try to match condition against the store
+		$queryResult = SMW\ApplicationFactory::getInstance()->getStore()->getQueryResult( $query );
+
+		if( $queryResult->getCount() === 0 ) {
+			# Create user
+			$backupWgUser = $wgUser;
+			$creator = $creator ?: $wgMGWikiDefaultCreatorNewAccounts;
+			$wgUser = User::newFromName( $creator );
+			if( !$wgUser || $wgUser->getId() === 0 ) {
+				if( $creator !== $wgMGWikiDefaultCreatorNewAccounts ) {
+					$wgUser = User::newFromName( $wgMGWikiDefaultCreatorNewAccounts );
+				} else {
+					throw new Exception(); // TODO improve
+				}
+			}
+			$adhAdepul = self::getUserFromOfficialADEPUL( $code_adepul );
+            if( !$adhAdepul ) {
+                throw new Exception(); // TODO improve
+            }
+			$prenom = $adhAdepul->prenom;
+			$nom = $adhAdepul->nom;
+			$mail = $adhAdepul->mail;
+			$profession = $adhAdepul->profession;
+			$specialite = $adhAdepul->specialite;
+			$username = $prenom . ' ' . strtoupper( $nom );
+			$userData = [];
+			$userData[$wgMGWikiUserProperties['firstname']] = $prenom;
+			$userData[$wgMGWikiUserProperties['lastname']] = $nom;
+			$userData[$wgMGWikiUserProperties['institution']] = Title::newFromText( 'ADEPUL', NS_PROJECT );
+			$userData[$wgMGWikiUserProperties['codeAdepul']] = $code_adepul;
+			$userData[$wgMGWikiUserProperties['email']] = $mail;
+			MGWiki::createUser( $username, $userData );
+			$wgUser = $backupWgUser;
+			$user = User::newFromName( $username );
+			return $user;
+		} elseif( $queryResult->getCount() > 1 ) {
+			throw new Exception(); // TODO improve
+		} elseif( $queryResult->getCount() === 1 ) {
+			$userValue = $queryResult->getResults()[0];
+			$username = $userValue->getDBkey();
+			$user = User::newFromName( $username );
+			return $user;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get an ADEPUL group.
+	 *
+	 * @param string $code_action ADEPUL action id.
+	 * @return Title|null MediaWiki page of the ADEPUL group.
+	 */
+	public static function getADEPULGroup( $code_action ) {
+		global $wgUser;
+		global $wgMGWikiUserProperties;
+
+		$codeActionTitle = Title::newFromText( 'Property:' . $wgMGWikiUserProperties['codeActionAdepul'] );
+		$codeAction = $codeActionTitle->getDBkey();
+
+		// Create property instance
+		$property = new SMWDIProperty( $codeAction );
+		$property->setPropertyTypeId( SMW\DataValues\StringValue::TYPE_ID );
+		$dataItem = new SMWDIBlob( $code_action );
+		$dataValue = SMW\DataValueFactory::getInstance()->newDataValueByItem(
+			$dataItem,
+			$property
+		);
+
+		// Create a description that represents the condition
+		$descriptionFactory = new SMW\Query\DescriptionFactory();
+		$description = $descriptionFactory->newSomeProperty(
+			$property,
+			$descriptionFactory->newValueDescription( $dataItem )
+		);
+		$propertyValue = SMW\DataValueFactory::getInstance()->newPropertyValueByLabel(
+			$codeAction
+		);
+
+		$description->addPrintRequest(
+			new SMW\Query\PrintRequest( SMW\Query\PrintRequest::PRINT_PROP, null, $propertyValue )
+		);
+
+		// Create query object
+		$query = new SMWQuery(
+			$description
+		);
+
+		$query->querymode = SMWQuery::MODE_INSTANCES;
+
+		// Try to match condition against the store
+		$queryResult = SMW\ApplicationFactory::getInstance()->getStore()->getQueryResult( $query );
+
+		if( $queryResult->getCount() === 0 ) {
+			return null;
+		} elseif( $queryResult->getCount() > 1 ) {
+			throw new Exception(); // TODO improve
+		} elseif( $queryResult->getCount() === 1 ) {
+			$groupValue = $queryResult->getResults()[0];
+			$group = Title::newFromText( $groupValue->getDBkey() );
+			return $group;
+		}
+
+		return null;
 	}
 }
