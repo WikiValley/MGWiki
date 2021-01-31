44ec1beb090cbebb5c07e43f0ebfe40ebb0f898f
Seb35 <seb35@seb35.fr>
2021-01-31 20:10:03 +0100

    Adaptation pour MediaWiki 1.33.
    Le hook pour modifier la boîte à outils a changé entre 1.33 et 1.35, la
    version pour 1.35 est donc prête en avance.

--- MGWikiSendNotification.php
+++ MGWikiSendNotification.php
@@ -19,7 +19,9 @@ class MGWikiSendNotification {
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$context = $out->getContext();
 		$action = \Action::getActionName( $context );
-		if( $out->isArticle() && $action === 'view' && $out->isRevisionCurrent() && $context->getRequest()->getVal( 'oldid' ) === null ) {
+		$revisionId = $out->getRevisionId();
+		if( $out->isArticle() && $action === 'view' && ( $revisionId == 0 || $revisionId == $out->getTitle()->getLatestRevID() ) && $context->getRequest()->getVal( 'oldid' ) === null ) {
+		#if( $out->isArticle() && $action === 'view' && $out->isRevisionCurrent() && $context->getRequest()->getVal( 'oldid' ) === null ) { // For 1.35+
 			$out->addModules( 'ext.mgwiki.send-notification' );
 		}
 	}
@@ -28,6 +30,8 @@ class MGWikiSendNotification {
 	 * Add the link in the toolbox to send a notification to the referrer.
 	 *
 	 * Only on articles when viewing the current revision
+	 *
+	 * This hook is for 1.35+.
 	 */
 	public static function onSidebarBeforeOutput( Skin $skin, &$sidebar ) {
 		$out = $skin->getOutput();
@@ -35,6 +39,28 @@ class MGWikiSendNotification {
 		$action = \Action::getActionName( $context );
 		if( $out->isArticle() && $action === 'view' && $out->isRevisionCurrent() && $context->getRequest()->getVal( 'oldid' ) === null ) {
 			$sidebar['TOOLBOX'][] = [
+				'msg' => 'mgwiki-toolbox-link',
+				'href' => '#',
+				'class' => 'mgwiki-send-notification',
+			];
+		}
+	}
+
+	/**
+	 * Add the link in the toolbox to send a notification to the referrer.
+	 *
+	 * Only on articles when viewing the current revision
+	 *
+	 * This hook is for 1.34-.
+	 */
+	public static function onBaseTemplateToolbox( $template, &$sidebar ) {
+		$skin = $template->getSkin();
+		$out = $skin->getOutput();
+		$context = $out->getContext();
+		$action = \Action::getActionName( $context );
+		$revisionId = $out->getRevisionId();
+		if( $out->isArticle() && $action === 'view' && ( $revisionId == 0 || $revisionId == $out->getTitle()->getLatestRevID() ) && $context->getRequest()->getVal( 'oldid' ) === null ) {
+			$sidebar['mgwiki-send-notification'] = [
 				'msg' => 'mgwiki-toolbox-link',
 				'href' => '#',
 				'class' => 'mgwiki-send-notification',
