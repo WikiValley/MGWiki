44aed795414f9159347eb688fbde7e962919a24e
Seb35 <seb35@seb35.fr>
2021-02-04 16:11:49 +0100

    Ajout d’une confirmation avant envoi de la notif.

--- resources/ext.mgwiki.send-notification.js
+++ resources/ext.mgwiki.send-notification.js
@@ -8,27 +8,46 @@
 			// Add the click action to the link
 			$( '#p-tb li.mgwiki-send-notification a, .mgwiki-send-notification-button' ).click( function( e ) {
 
-				// Trigger the API action
-				new mw.Api().postWithToken( 'csrf', {
-					action: 'mgwiki-send-notification',
-					title: mw.config.get( 'wgPageName' ),
-					errorformat: 'plaintext'
-				} )
+				var confirmationDialog = new OO.ui.MessageDialog();
 
-				// It worked: acknowledge to the user
-				.then( function() {
-					mw.notify( mw.message( 'mgwiki-notification-success' ), { title: mw.message( 'mgwiki-notification-title' ), type: 'success' } );
+				// Create and append a window manager.
+				var windowManager = new OO.ui.WindowManager();
+				$( 'body' ).append( windowManager.$element );
 
-				// It failed: display the error to the user
-				}, function( code, result ) {
-					let error = result && result.errors && result.errors[0] && result.errors[0].code && result.errors[0]['*'] ? result.errors[0] : '';
-					if( error && error.code === 'mgwiki-error-when-sending-email' ) {
-						mw.notify( result.errors[0]['*'], { title: mw.message( 'mgwiki-notification-title' ), type: 'warn', autoHide: false } );
-					} else if( error ) {
-						mw.notify( result.errors[0]['*'], { title: mw.message( 'mgwiki-notification-title' ), type: 'error', autoHide: false } );
-					} else {
-						mw.notify( mw.message( 'mgwiki-notification-unknown-error' ), { title: mw.message( 'mgwiki-notification-title' ), type: 'error', autoHide: false } );
+				// Add the dialog to the window manager.
+				windowManager.addWindows( [ confirmationDialog ] );
+
+				// Configure the message dialog when it is opened with the window manager's openWindow() method.
+				windowManager.openWindow( confirmationDialog, {
+					title: mw.message( 'mgwiki-send-notification-confirm-title' ).text(),
+					message: mw.message( 'mgwiki-send-notification-confirm-top' ).text()
+				} ).closed.then( function ( data ) {
+					if( !data || data.action !== 'accept' ) {
+						return;
 					}
+
+					// Trigger the API action
+					new mw.Api().postWithToken( 'csrf', {
+						action: 'mgwiki-send-notification',
+						title: mw.config.get( 'wgPageName' ),
+						errorformat: 'plaintext'
+					} )
+
+					// It worked: acknowledge to the user
+					.then( function() {
+						mw.notify( mw.message( 'mgwiki-notification-success' ), { title: mw.message( 'mgwiki-notification-title' ), type: 'success' } );
+
+						// It failed: display the error to the user
+						}, function( code, result ) {
+							let error = result && result.errors && result.errors[0] && result.errors[0].code && result.errors[0]['*'] ? result.errors[0] : '';
+						if( error && error.code === 'mgwiki-error-when-sending-email' ) {
+							mw.notify( result.errors[0]['*'], { title: mw.message( 'mgwiki-notification-title' ), type: 'warn', autoHide: false } );
+						} else if( error ) {
+							mw.notify( result.errors[0]['*'], { title: mw.message( 'mgwiki-notification-title' ), type: 'error', autoHide: false } );
+						} else {
+							mw.notify( mw.message( 'mgwiki-notification-unknown-error' ), { title: mw.message( 'mgwiki-notification-title' ), type: 'error', autoHide: false } );
+						}
+					} );
 				} );
 
 				e.preventDefault();
